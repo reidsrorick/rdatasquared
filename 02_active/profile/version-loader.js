87b7045d96faed8816f43b4e11dc95f8d86e0146
reidsrorick <123706323+reidsrorick@users.noreply.github.com>
2025-06-03 14:57:59 -0500

    versioning test 2

--- 02_active/profile/version-loader.js
+++ 02_active/profile/version-loader.js
@@ -1,9 +1,8 @@
-
 fetch("/02_active/profile/version.json")
   .then(res => res.json())
   .then(data => {
     const versionDiv = document.getElementById("version");
     if (versionDiv) {
-      versionDiv.textContent = `Version ${data.version} — Last updated ${data.lastUpdated}`;
+      versionDiv.textContent = data.footer;
     }
   });
